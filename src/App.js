2677466874332ac8284badcbccadf0f0a2bd7d98
Justin <justin_jang@brown.edu>
2024-06-19 14:31:42 -0700

    button

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
   const [cats, setCats] = useState([]);
 
   const addCat = () => {
-    setCats([...cats, <Cat key={cats.length} />]);
+    setCats([...cats, <Cat key={cats.length}/>]);
   };
 
   return (
     <div className="App">
-    <button onClick={addCat}>Add kitty</button>
+    <button onClick={addCat} className='button-56' role='button'>Add kitty</button>
     <div className="cat-container">
       {cats}
     </div>
--- src/components/cat.js
+++ src/components/cat.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './cat.css'
 import kitty from '../img/kitty.svg';
  const Cat = () => {
+
     return <div className='cat'>
         <img src={kitty} alt='kitty'></img>
     </div>
